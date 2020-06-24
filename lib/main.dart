8d7dde8de47a3a5adcc8f3ebb914c46fc57e7731
RAJARSHI <rajarshibz@gmail.com>
2020-06-24 20:45:40 +0530

    Added dartChange() function

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,64 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.lightGreen,
         appBar: AppBar(
           title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          backgroundColor: Colors.green.shade900,
         ),
         body: DicePage(),
       ),
     ),
   );
 }
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
 
-class DicePage extends StatelessWidget {
+class _DicePageState extends State<DicePage> {
+  var leftDiceNumber =1;
+  var rightDiceNumber =1;
+
+  final _random = new Random();
+
+  void diceChange(){
+    setState(() {
+      rightDiceNumber = 1 + _random.nextInt(6);
+      leftDiceNumber = 1 + _random.nextInt(6);
+    });
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+              onPressed: (){
+                diceChange();
+
+              },
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+              onPressed: (){
+                diceChange();
+
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
